17c3c8330130e74c9d660f14692dbe76ba6452b5
Henry <henrijs.sprogis@pm.me>
2021-12-08 12:56:33 +0200

    Switch back adapter to auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import cloudflare from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: cloudflare({}),
+		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: "#svelte",
