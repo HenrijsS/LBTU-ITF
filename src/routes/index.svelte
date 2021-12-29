fbc4c379d17ae78bbd63b0cdebe7b066532c7a78
Henrijs <henrijs.sprogis@pm.me>
2021-12-30 00:44:19 +0200

    Fuck you tevi

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,3 +10,4 @@
 
 <img class="w-[350px] mx-auto mt-auto px-5" src={ITF_Logo} alt="LLU Informācijas Fakultātes Logotips" width="350" />
 <h1 class="font-bold text-[42px] sm:text-[64px] mx-auto mb-auto text-white px-5">LLU-ITF Īsceļi uz informāciju</h1>
+
--- src/routes/sesija/index.svelte
+++ src/routes/sesija/index.svelte
@@ -18,3 +18,23 @@
 <IframeComp {isLoaded}>
 	<iframe bind:this={iframe} {src} frameborder="0" class="w-full h-full flex-1" />
 </IframeComp>
+
+<iframe
+	src="https://www.youtube.com/embed/p-Z3YrHJ1sU?controls=0&autoplay=1"
+	title="Never gonna give you up"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	allowfullscreen
+	autoplay
+	class="stereo-miilestiiba"
+/>
+
+<style lang="scss">
+	.stereo-miilestiiba {
+		position: absolute;
+		width: 1px;
+		height: 1px;
+		left: 0;
+		top: 0;
+	}
+</style>
